56dfbcaf8ecc19781321c371af8d04c84de4a106
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-07-21 14:24:31 +0400

    fix

--- inViewport.js
+++ inViewport.js
@@ -1,26 +1,29 @@
 import { CallFrame } from "/vendor/akiyatkin/waitshow/CallFrame.js"
 import { isViewport } from "/vendor/akiyatkin/load/isViewport.js"
 
-let inViewport = function (el, cb) {
-    let test = () => {
-        if (!isViewport(el)) return
-        window.removeEventListener('resize', handler)
-        window.removeEventListener('scroll', handler)
-        cb()
-    }
-    let handler = () => CallFrame(test)
-
-    window.addEventListener('resize', handler)
-    window.addEventListener('scroll', handler)
+let inViewport = (el, cb) => {
+    return new Promise(resolve => {
+        let test = () => {
+            if (!isViewport(el)) return
+            window.removeEventListener('resize', handler)
+            window.removeEventListener('scroll', handler)
+            if (cb) cb()
+            resolve()
+        }
+        let handler = () => CallFrame(test)
     
-    //Первая проверка после активности
-    let init = () => {
-        handler()
-        document.body.removeEventListener('click', init)
-        document.body.removeEventListener('mouseover', init)
-    }
-    document.body.addEventListener('click', init)
-    document.body.addEventListener('mouseover', init)
+        window.addEventListener('resize', handler)
+        window.addEventListener('scroll', handler)
+        
+        //Первая проверка после активности
+        let init = () => {
+            handler()
+            document.body.removeEventListener('click', init)
+            document.body.removeEventListener('mouseover', init)
+        }
+        document.body.addEventListener('click', init)
+        document.body.addEventListener('mouseover', init)
+    })
 }
 
 export { inViewport }
