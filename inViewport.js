1bd1988656a25373b41a838302cb3bea09ea9940
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2020-10-04 17:16:30 +0400

    fix

--- inViewport.js
+++ inViewport.js
@@ -2,28 +2,30 @@ import { CallFrame } from "/vendor/akiyatkin/waitshow/CallFrame.js"
 import { isViewport } from "/vendor/akiyatkin/load/isViewport.js"
 
 let second = false
-let inViewport = (el, cb) => {
+const inViewport = (el, cb) => {
     return new Promise(resolve => {
-        let test = () => {
+        const hand = () => {
             if (!isViewport(el)) return
             window.removeEventListener('resize', handler)
             window.removeEventListener('scroll', handler)
+            document.body.removeEventListener('click', init)
+            document.body.removeEventListener('mouseover', init)
             if (cb) cb()
             resolve()
         }
-        let handler = () => CallFrame(test)
-    
-        window.addEventListener('resize', handler)
-        window.addEventListener('scroll', handler)
-        
-        //Первая проверка после активности или если были переходы по сайту
-        if (second) return handler()
-        let init = () => {
+        const handler = () => CallFrame(hand)
+        const init = () => {
             second = true
             handler()
             document.body.removeEventListener('click', init)
             document.body.removeEventListener('mouseover', init)
         }
+
+        window.addEventListener('resize', handler)
+        window.addEventListener('scroll', handler)
+        
+        //Первая проверка после активности или сразу при следующей проверке
+        if (Crumb.counter < 2) return handler()
         document.body.addEventListener('click', init)
         document.body.addEventListener('mouseover', init)
     })
