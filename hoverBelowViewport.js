c9bff506d984bd137c8e1167be8f969656bcb517
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2021-06-11 16:07:42 +0400

    below

---
+++ hoverBelowViewport.js
@@ -0,0 +1,39 @@
+import { CallFrame } from "/vendor/akiyatkin/waitshow/CallFrame.js"
+import { isBelowViewport } from "/vendor/akiyatkin/load/isBelowViewport.js"
+let second = false
+const hoverBelowViewport = (el, over, out) => {
+    let isover = false
+    let isout = false
+    if (!el) return
+    const handler = () => CallFrame(() => {
+        if (isBelowViewport(el)) {
+            if (!isover) {
+                isover = true
+                isout = false
+                over()
+            }
+        } else {
+            if (!isout) {
+                isout = true
+                isover = false
+                out()
+            }
+        }
+    })
+    window.addEventListener('resize', handler)
+    window.addEventListener('scroll', handler)
+    const init = () => {
+        second = true
+        handler()
+        document.body.removeEventListener('click', init)
+        document.body.removeEventListener('mouseover', init)
+    }
+    if (!second) {
+        document.body.addEventListener('click', init)
+        document.body.addEventListener('mouseover', init)
+    } else {
+        handler()
+    }
+}
+
+export { hoverBelowViewport }
